af882c8d2b6cd6be9b6688f14fc3f42c2b96c18e
Victor Fors <krakow89@gmail.com>
2015-07-01 20:19:52 +0200

    Refactoring

--- pcap.hs
+++ pcap.hs
@@ -3,6 +3,7 @@ module Main where
 import Network.Pcap
 import Data.ByteString
 import Data.Char
+import Control.Applicative
 
 -- Copyright Bo Victor Natanael Fors <krakow89@gmail.com>
 -- This program is free software: you can redistribute it and/or modify
@@ -28,5 +29,8 @@ showPacket :: PktHdr -> ByteString -> IO ()
 showPacket header bstr = Prelude.putStrLn $ transformPacket bstr
 
 transformPacket :: ByteString -> String
-transformPacket bstr = Prelude.filter isPrint . Prelude.filter isAscii
-                       . fmap chr . fmap fromIntegral . unpack $ bstr
+transformPacket bstr = Prelude.filter isPrint
+                       . Prelude.filter isAscii
+                       . fmap chr
+                       . fmap fromIntegral
+                       . unpack $ bstr
