a47394d3838d16de0b1b7a81bdb47e4328cd4167
Victor Fors <krakow89@gmail.com>
2015-07-02 18:07:13 +0200

    Refactoring.

--- arpSniffer.hs
+++ arpSniffer.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Network.Pcap
-import Data.ByteString
+import Data.ByteString (ByteString, unpack)
 import Data.Char
 import System.IO (hFlush, stdout)
 import Numeric (showHex)
@@ -27,14 +27,14 @@ import Control.Monad (void)
 main :: IO ()
 main = void (getArgs >>= help program)
   where help :: ([String] -> IO ()) -> [String] -> IO ()
-        help _ [] = Prelude.putStrLn "Usage: arpSniffer <device>"
+        help _ [] = putStrLn "Usage: arpSniffer <device>"
         help fn argv = fn argv
         
 -- | Open the device and set up the handler.
 program :: [String] -> IO ()
 program (device:_) = openLive device 2048 True 10000
                      >>= handlePackets
-                     >>= print
+                     >>= putStrLn . ("Captured packets: " ++) . show 
 program _ = return ()
 
 -- | Set up the handler.
@@ -43,13 +43,13 @@ handlePackets handle = loopBS handle (-1) showPacket
 
 -- | Packet handler, called for every read packet.
 showPacket :: PktHdr -> ByteString -> IO ()
-showPacket _ bstr = maybe (return ()) Prelude.putStrLn (transformPacket bstr)
+showPacket _ bstr = maybe (return ()) putStrLn (transformPacket bstr)
                     >> hFlush stdout
 
 -- | First layer dissector, drop ethernet header.
 transformPacket :: ByteString -> Maybe String
 transformPacket bstr = handlePacket
-                       . Prelude.drop 14
+                       . drop 14
                        . fmap fromIntegral
                        . unpack $ bstr
 
@@ -63,7 +63,7 @@ handlePacket (0:1:8:0:6:4:0:oper:rest) | oper == 1 =
                                            $ handle' rest
                                        | otherwise = Nothing
   where handle' :: [Int] -> Maybe String
-        handle' packetHeader | Prelude.length packetHeader == 20 =
+        handle' packetHeader | length packetHeader == 20 =
                                  Just
                                  $ showMac packetHeader 0
                                  ++ " "
@@ -76,7 +76,7 @@ handlePacket _ = Nothing
 
 -- | Pad a string to a given length.
 padRight :: Int -> String -> String
-padRight len str = str ++ Prelude.replicate (len - Prelude.length str) ' '
+padRight len str = str ++ replicate (len - length str) ' '
 
 -- | Pad a byte in hex format with zeroes to 2 chars width.
 formatHex :: String -> String
@@ -86,16 +86,16 @@ formatHex ax = ax
 -- | Display a HW address from a certain offset in a packet header.
 showMac :: [Int] -> Int -> String
 showMac packetHeader offset = fmap toUpper
-                              $ Data.List.intercalate ":"
+                              $ intercalate ":"
                               $ fmap (formatHex . (`showHex` ""))
-                              $ Prelude.take 6
-                              $ Prelude.drop offset packetHeader
+                              $ take 6
+                              $ drop offset packetHeader
 
 -- | Display an IPv4 adress from a certain offset in a packet header.
 showIp :: [Int] -> Int -> String
 showIp packetHeader offset = padRight ((3 * 4) + 2)
-                             $ Data.List.intercalate "."
+                             $ intercalate "."
                              $ fmap show
-                             $ Prelude.take 4
-                             $ Prelude.drop offset packetHeader
+                             $ take 4
+                             $ drop offset packetHeader
     
